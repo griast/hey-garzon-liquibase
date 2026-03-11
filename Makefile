1065519c9291993a0bbea99ad6a871bbc4e89d0c
Gustavo Rivas <gustavo.alexis.ra@gmail.com>
2026-03-10 22:40:54 -0300

    #2 feat: populate db with business data

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 show-diagram:
 	open src/main/resources/db/model.html
+
+clear-database:
+	./gradlew bootRun --args='--spring.liquibase.dropFirst=true --db.loadInitialData=true'
--- src/main/resources/dbinit/data.sql
+++ src/main/resources/dbinit/data.sql
@@ -18,3 +18,19 @@ insert into "user_role" (id_user, id_role) values ((select id from "user" where 
 insert into "user_role" (id_user, id_role) values ((select id from "user" where name = 'Martin Business Admin'), (select id from "role" where name = 'BUSINESS_ADMIN'));
 insert into "user_role" (id_user, id_role) values ((select id from "user" where name = 'Sebastian Business Admin'), (select id from "role" where name = 'BUSINESS_ADMIN'));
 insert into "user_role" (id_user, id_role) values ((select id from "user" where name = 'Amanda Root'), (select id from "role" where name = 'ROOT_ADMIN'));
+
+
+insert into "business" (name, address, phones, mail, logo, enabled) values ('Business 1', 'Address 1', '1234567890', 'business1@example.com', 'https://example.com/logo1.png', true);
+insert into "business" (name, address, phones, mail, logo, enabled) values ('Business 2', 'Address 2', '1234567890', 'business2@example.com', 'https://example.com/logo2.png', true);
+insert into "business" (name, address, phones, mail, logo, enabled) values ('Business 3', 'Address 3', '1234567890', 'business3@example.com', 'https://example.com/logo3.png', true);
+
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 1'), 'https://example.com/social1.png', 'FACEBOOK');
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 1'), 'https://example.com/social2.png', 'INSTAGRAM');
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 2'), 'https://example.com/social3.png', 'FACEBOOK');
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 2'), 'https://example.com/social4.png', 'INSTAGRAM');
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 3'), 'https://example.com/social5.png', 'FACEBOOK');
+insert into "business_social_media" (id_business, url, platform) values ((select id from "business" where name = 'Business 3'), 'https://example.com/social6.png', 'INSTAGRAM');
+
+insert into "business_user" (id_business, id_user) values ((select id from "business" where name = 'Business 1'), (select id from "user" where name = 'Roberto Business Admin'));
+insert into "business_user" (id_business, id_user) values ((select id from "business" where name = 'Business 2'), (select id from "user" where name = 'Nicolas Business Admin'));
+insert into "business_user" (id_business, id_user) values ((select id from "business" where name = 'Business 3'), (select id from "user" where name = 'Martin Business Admin'));
